b6cb93451ad318887d5cf99fadeb1f9304cab2cb
Blake Ellingham <blake.ellingham@rocksaucestudios.com>
2014-08-27 17:08:55 -0500

    Started with Django

---
+++ Django/firstProject/FIRST_PROJ/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/blakeellingham/Documents/IndependentLearning/Learn_python/Django/firstProject/FIRST_PROJ/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
